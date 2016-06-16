c5390d7560fca1886e7cb76ba26f8238bee19140
admin <admin@macbook1-6.local>
2016-06-16 17:03:53 +0800

    Initial Commit

---
+++ TCDSCarouselFigure-Demo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  TCDSCarouselFigure-Demo
+//
+//  Created by admin on 16/6/16.
+//  Copyright © 2016年 TCDS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
